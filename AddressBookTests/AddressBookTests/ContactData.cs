35608b5d46288aff716065622960fa5cfea5a124
alexshkitin <azazello92@yandex.ru>
2019-02-17 20:38:05 +0300

    Add test for contact creation, add new class with contact data.

---
+++ AddressBookTests/AddressBookTests/ContactData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookTests
+{
+    class ContactData
+    {
+        private string firstName;
+        private string lastName;
+
+        public ContactData(string firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        public string FistName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+    }
+}
