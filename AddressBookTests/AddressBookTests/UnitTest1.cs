81854b773f088b5f39e3a7209a96629946b1884d
alexshkitin <azazello92@yandex.ru>
2019-02-17 18:32:03 +0300

    Added first test for group creation.

--- AddressBookTests/AddressBookTests/UnitTest1.cs
+++ AddressBookTests/AddressBookTests/UnitTest1.cs
@@ -1,14 +1,1 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace AddressBookTests
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
-    }
-}
+﻿
